5ee03c50ff9ae7e8007b4c6b31a2c413c6d5ba53
Gbaja <gbajaf@yahoo.co.uk>
2018-08-14 16:47:45 +0100

    Added text input and Button
    There was not a way to enter and add new to do. Add a text input and a
    add button to app.

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      todoInput: "",
+      toDos: []
+    }
+  }
+  addTodo(item){
+    console.log("ITEM: ", item)
+    // const addToDoToArray = this.state.toDos.concat(item)
+    this.setState({toDos: [...this.state.toDos, item]})
+    console.log(this.state.toDos)
+  
+  }
   render() {
+    console.log("TODO'S ",this.state.toDos)
     return (
       <View style={styles.container}>
         <Text>To Do App</Text>
+        <TextInput 
+          style={{height: 40, width: 300, borderColor: "red", borderWidth: 2, marginTop: 15}}
+          value={this.state.todoInput}
+          onChangeText={(text)=>this.setState({todoInput: text})}
+          />
+          <Button
+          onPress={()=> this.addTodo(this.state.todoInput)}
+          title="Add to do"/>
       </View>
     );
   }
